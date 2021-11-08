6ec49bae2b92e68b8e041114afc1924b734ee063
Zhen Ju <juzhenatpku@gmail.com>
2021-11-08 21:10:07 +0800

    [TMP] Create and close connection each time pushing message

--- common/producer.py
+++ common/producer.py
@@ -10,16 +10,21 @@ EXCHANGE_NAME = 'delayed_exchange'
 
 credentials = pika.PlainCredentials(RMQ_USER, RMQ_PASS)
 
-channel = None
 def init():
-    global channel
-    conn = pika.BlockingConnection(pika.ConnectionParameters(RMQ_HOST, virtual_host=RMQ_VHOST, credentials=credentials))
-    channel = conn.channel()
+    #global channel
+    #conn = pika.BlockingConnection(pika.ConnectionParameters(RMQ_HOST, virtual_host=RMQ_VHOST, credentials=credentials, heartbeat=5))
+    #channel = conn.channel()
+    #channel.basic_qos(prefetch_count=1)
 
-    channel.exchange_declare(EXCHANGE_NAME, 'x-delayed-message', arguments={'x-delayed-type':'direct'})
-    channel.queue_declare(queue=QUEUE_NAME)
+    #channel.exchange_declare(EXCHANGE_NAME, 'x-delayed-message', arguments={'x-delayed-type':'direct'}, durable=True, auto_delete=True)
+    #channel.queue_declare(queue=QUEUE_NAME, durable=True, auto_delete=True, arguments={'x-queue-mode':'lazy'})
+    pass
 
 def push_event(event_body, delay=0):
+    conn = pika.BlockingConnection(pika.ConnectionParameters(RMQ_HOST, virtual_host=RMQ_VHOST, credentials=credentials, heartbeat=5))
+    channel = conn.channel()
+    channel.basic_qos(prefetch_count=1)
     delay_props = pika.BasicProperties(headers={'x-delay': delay})
     channel.basic_publish(exchange=EXCHANGE_NAME, routing_key=QUEUE_NAME, body=event_body, properties=delay_props)
+    conn.close()
 
