0f0274eeb86ed4692327e8521b2eccaa3037dfd0
Zhen Ju <juzhenatpku@gmail.com>
2021-12-07 10:02:31 +0000

    Update the date according to the new module designs.

--- resources/data_agent.py
+++ resources/data_agent.py
@@ -17,35 +17,52 @@ class Issue(Resource):
         #  TODO This won't work, the rule handler will parse
         #  it into an empty string, find out why
         #  res = requests.post(RULE_HANDLER_URL, json=json.dumps(request.json, indent=4))
-        res = requests.post(RULE_HANDLER_URL, json=request.json)
+
+
+        rule_req_obj = {
+            'issueID': request.json['issueID'],
+            'issueAction': request.json['issueAction'],
+            'issueUser': request.json['issueUser'],
+            'issueTime': request.json['issueTime'],
+            'issueUpdateTime': request.json['issueUpdateTime'],
+            'issueAssignee': request.json['issueAssignee'],
+            'issueLabel': request.json['issueLabel'],
+            'issueTitle': request.json['issueTitle'],
+            'issueContent': request.json['issueContent'],
+        }
+
+        res = requests.post(RULE_HANDLER_URL, json=rule_req_obj)
         if res.status_code >= 300:
             logger.error(f'error status_code: {res.status_code}, content: {res.text}')
             return "Internal Error", 500
-        rule_json = res.json()[0] # TODO Is the rule object a list or dict?
-        logger.debug(json.dumps(rule_json, indent=4))
-        #  TODO Construct the Event Object with rule and issue info
-        rule_type = rule_json['ruleType']
-        target_info = rule_json['infoPayload'] if rule_type == 'info' else ""
-        target_label = "" # TODO Where to get the value?
-        target_assignee_id = "" # TODO Where to get the value?
 
-        event_obj = {
-            'issueID': rule_json['issueID'],
-            'eventType': rule_json['ruleType'],
-            'targetInfo': target_info,
-            'targetLabel': target_label,
-            'targetAssigneeID': target_assignee_id,
-            'pushTime': rule_json['exeTime'],
-        }
+        for rule_json in res.json():
+            #rule_json = res.json()[0] # TODO Is the rule object a list or dict?
+            logger.debug(json.dumps(rule_json, indent=4))
+            #  TODO Construct the Event Object with rule and issue info
+            rule_type = rule_json['ruleType']
+            target_info = rule_json['infoPayload'] if rule_type == 'info' else ""
+            target_label = "" # TODO Where to get the value?
+            target_assignee_id = "" # TODO Where to get the value?
 
-        logging.debug(json.dumps(event_obj, indent=4))
+            event_obj = {
+                'issueID': rule_json['issueID'],
+                'eventType': rule_json['ruleType'],
+                'targetInfo': target_info,
+                'targetLabel': target_label,
+                'targetAssigneeID': target_assignee_id,
+                'pushTime': rule_json['exeTime'],
+            }
 
-        # TODO Get delay by comparing the current time and event_obj.pushTime
-        target_time = datetime.strptime(rule_json['exeTime'], "%Y-%m-%dT%H:%M:%S%z")
-        now = datetime.now(tz=target_time.tzinfo)
-        diff = target_time - now
-        delay = 0 if diff.total_seconds() <= 0 else diff.total_seconds()
-        push_event(json.dumps(event_obj), delay=delay)
+
+            logging.debug(json.dumps(event_obj, indent=4))
+
+            # TODO Get delay by comparing the current time and event_obj.pushTime
+            target_time = datetime.strptime(rule_json['exeTime'], "%Y-%m-%dT%H:%M:%S%z")
+            now = datetime.now(tz=target_time.tzinfo)
+            diff = target_time - now
+            delay = 0 if diff.total_seconds() <= 0 else diff.total_seconds()
+            push_event(json.dumps(event_obj), delay=delay)
 
         #executor_res = requests.post(STRATEGY_EXECUTOR_URL, json.dumps(event_obj))
         #logging.debug(f'res.status: {executor_res.status_code}')
